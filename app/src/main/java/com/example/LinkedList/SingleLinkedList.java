b9470a9d38028c1e7cd03de43319ba90d18aabd8
cjh <529836078@qq.com>
2021-04-16 18:45:02 +0800

    单向链表

--- app/src/main/java/com/example/LinkedList/SingleLinkedList.java
+++ app/src/main/java/com/example/LinkedList/SingleLinkedList.java
@@ -70,7 +70,125 @@ public class SingleLinkedList {
             topNode = topNode.next;
         }
         topNode.next = newNode;
+    }
 
+    // 在节点P后面插入
+    public void insertAfter(Node p, int value) {
+        if(p == null) return;
+        Node newNode = new Node(value, null);
+        newNode.next = p.next;
+        p.next = newNode;
+    }
+
+    // 在节点P前面插入
+    public void insertBefore(Node p, int value) {
+        if (p == null) return;
+        // 如果p节点是头结点
+        if(p.equals(head)) {
+            insertAtHead(value);
+            return;
+        }
+        Node newNode = new Node(value, null);
+        // 找到P的前面一个节点
+       Node pPre = head;
+       while (pPre != null && !pPre.next.equals(p)) {
+           pPre = pPre.next;
+       }
+       if(pPre!=null) {
+           pPre.next = newNode;
+           newNode.next = p;
+       }
+    }
+
+    // 删除节点
+    public void deleteNode(Node p) {
+        if(p == null || head == null) return;
+        if(head.equals(p)) {    // 如果要删除的节点是链头
+            head = head.next;
+            return;
+        }
+        // 找出要删除节点的前一个节点
+        Node beforeOfDeleteNode = head;
+        while (beforeOfDeleteNode != null && !beforeOfDeleteNode.next.equals(p)) {
+            beforeOfDeleteNode = beforeOfDeleteNode.next;
+        }
+        if(beforeOfDeleteNode != null) {
+            beforeOfDeleteNode.next = p.next;
+        }
+    }
+
+    public void deleteByValue(int value) {
+        if(head == null) return;
+        Node p = head;
+        while (p != null && p.next != null) {
+            if(p.next.data == value) {
+                p.next = p.next.next;
+            }
+            p = p.next;
+        }
+        if(head.data == value) {
+            head = head.next;
+        }
+    }
+
+    public void printList() {
+        Node newNode = head;
+        System.out.print("LinkedList::");
+        while (newNode != null) {
+            System.out.print(newNode.data+ " ");
+            newNode = newNode.next;
+        }
+        System.out.println("");
+    }
+
+    // 是否为回文
+    public boolean palindrome() {
+        if(head == null) return false;
+        if(head.next == null) return true; // 链表只有一个节点
+        Node top = head;
+        int topIndex = 0;
+        while (top.next != null) {
+            top = top.next;
+            topIndex ++;
+        }
+        int size = topIndex + 1;
+        Node h = head;
+        int hIndex = 0;
+
+
+        boolean isEven = size % 2 == 0;
+        int center = size / 2;
+        while (isEven ? hIndex-topIndex!=1 : !(topIndex == center && center == hIndex)) {
+            if(h.data != top.data) {
+                return false;
+            }
+            hIndex ++;
+            h = h.next;
+            topIndex --;
+            top = findByIndex(topIndex);
+        }
+
+        return true;
+    }
+
+    public Node inverseLinkList(Node p){
+        if(head == null) return null;       // 链表为空
+        if(head.next == null) return new Node(head.data, null); // 链表只有一个链头
+        // 找出p的前一个结点
+        Node originCurrent = p;
+        Node originNode = head;
+        Node newListHead = new Node(p.data, null);
+        Node newListNode = null;
+
+        while (!originCurrent.equals(head)) {
+//            while (originCurrent.next.equals())
+        }
+
+//        while (!beforeOfP.next.equals(p)) {
+//            beforeOfP = beforeOfP.next;
+//        }
+//        if(beforeOfP == null) return null;  // 当前链表没有p节点
+//        head.next = beforeOfP;
     }
 
     public static void test() {
@@ -80,35 +198,76 @@ public class SingleLinkedList {
         list.insert(2);
         list.insert(3);
         list.insert(4);
-        list.insert(5);
-        list.insertAtHead(-1);
-        Node node =list.findByValue(0);
-        if(node == null) {
-            System.out.println("LinkedList::没有找到值为0的节点");
-        }
-        else {
-            System.out.println("LinkedList::找到值为0的节点::"+node.data);
-        }
-        node =list.findByValue(5);
-        if(node == null) {
-            System.out.println("LinkedList::没有找到值为5的节点");
-        }
-        else {
-            System.out.println("LinkedList::找到值为5的节点::"+node.data);
-        }
+//        list.insert(1);
+//        list.insert(0);
+//        list.insertAtHead(-1);
+//        Node node =list.findByValue(0);
+//        if(node == null) {
+//            System.out.println("LinkedList::没有找到值为0的节点");
+//        }
+//        else {
+//            System.out.println("LinkedList::找到值为0的节点::"+node.data);
+//        }
+//        node =list.findByValue(5);
+//        if(node == null) {
+//            System.out.println("LinkedList::没有找到值为5的节点");
+//        }
+//        else {
+//            System.out.println("LinkedList::找到值为5的节点::"+node.data);
+//        }
+//
+//        Node node0 = list.findByIndex(0);
+//        if(node0 != null) {
+//            System.out.println("LinkedList::index=0::node=" + node0.data);
+//        }
+//        node = list.findByIndex(5);
+//        if(node != null) {
+//            System.out.println("LinkedList::index=5::node=" + node.data);
+//        }
+//        Node node6 = list.findByIndex(6);
+//        if(node6 != null) {
+//            System.out.println("LinkedList::index=6::node=" + node6.data);
+//        }
+//
+//        list.printList();
+//
+//        list.insertAfter(node6, 7);
+//        System.out.print("insertAfter::");
+//        list.printList();
+//        list.insertAfter(node0, -2);
+//        System.out.print("insertAfter::");
+//        list.printList();
+//
+//        list.insertBefore(node0, -3);
+//        System.out.print("insertBefore::");
+//        list.printList();
+//        list.insertBefore(list.findByIndex(9), 8);
+//        System.out.print("insertBefore::");
+//        list.printList();
+//
+//        list.deleteNode(list.findByIndex(0));
+//        System.out.print("deleteNode::");
+//        list.printList();
+//        list.deleteNode(list.findByIndex(9));
+//        System.out.print("deleteNode::");
+//        list.printList();
+//
+//        list.insert(-1);
+//        System.out.print("insert::");
+//        list.printList();
+//        list.deleteByValue(-1);
+//        System.out.print("deleteByValue::");
+//        list.printList();
+//
+//        list.deleteByValue(8);
+//        System.out.print("deleteByValue::");
+//        list.printList();
+        list.printList();
+        boolean result = list.palindrome();
+        System.out.println("LinkedList::palindrome::" + result);
 
-        node = list.findByIndex(0);
-        if(node != null) {
-            System.out.println("LinkedList::index=0::node=" + node.data);
-        }
-        node = list.findByIndex(5);
-        if(node != null) {
-            System.out.println("LinkedList::index=5::node=" + node.data);
-        }
-        node = list.findByIndex(6);
-        if(node != null) {
-            System.out.println("LinkedList::index=6::node=" + node.data);
-        }
+        Node node = list.inverseLinkList(list.findByIndex(2));
+        list.printList();
     }
 
 }
